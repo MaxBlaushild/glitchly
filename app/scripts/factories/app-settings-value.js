2487fd4192ddf7e4340c4919237425d74207c126
MaxBlaushild <blaushmild@gmail.com>
2015-09-29 20:14:38 -0400

    Adds front end functionality that allows you to recieve notifications in real time

--- app/scripts/factories/app-settings-value.js
+++ app/scripts/factories/app-settings-value.js
@@ -3,6 +3,7 @@
 (function(){
 
   angular.module("frontendApp").value('appSettings', {
+    // apiUrl: "http://159.203.86.38",
     apiUrl: "http://localhost:3000",
     version: "1.0"
   });
---
+++ app/scripts/factories/watcher-factory.js
@@ -0,0 +1,65 @@
+'use strict';
+
+(function(){
+
+  var watcherFactory = function(){
+
+    var createWatcher = function(url, conf){
+
+      var token = function(conf) {
+        return conf && (conf = conf.Authorization) &&
+        (conf = typeof conf === 'string' &&
+        conf.split('=')) &&
+        Array.isArray(conf) && conf[1];
+      };
+
+      url += '?token=' + token(conf);
+      url += conf.timeout ? '&timeout=' + conf.timeout : '';
+      var es = new EventSource(url);
+
+      var close = function() {
+        es.close();
+      };
+
+      var makeHandler = function(handler, close) {
+        return function(e) {
+          if (close) {
+            close();
+          }
+          return handler(e.data ? e.data : e);
+        };
+      };
+
+      var on = function(event, handler) {
+        switch (event) {
+          case 'connect':
+            es.onopen = makeHandler(handler);
+            break;
+          case 'change':
+            es.onmessage = makeHandler(handler);
+            break;
+          case 'error':
+            es.onerror = makeHandler(handler, close);
+            break;
+          default:
+            console.error('Unknown event type:' + event);
+            break;
+        }
+      };
+
+      return {
+        close: close,
+        on: on
+      };
+    }
+
+    return {
+      createWatcher: createWatcher
+    };
+
+  }
+
+  angular.module("frontendApp").factory('WatcherFactory', watcherFactory);
+  watcherFactory.$inject = [];
+
+})();
